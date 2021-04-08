c418d2145b1895f7dd87c217c653acd31dfd3a60
Rex Elardo <rexelardo@gmail.com>
2021-04-08 17:47:24 -0400

    added the get_data into a function, so it can be called from another Python file

--- data.py
+++ data.py
@@ -1,15 +1,20 @@
 import pandas as pd
 import numpy as np
-X = pd.read_csv('raw_data/test_data_X.csv')
-y_test = pd.read_csv('raw_data/test_data_y.csv')
-
-X = X.drop(columns='Unnamed: 0')
-
-X = np.array(X)
-X = X.reshape(1413,24,5)
-X
 from tensorflow.keras.models import load_model
-new_model  = load_model('gs://horseracingproject/base_model')
+# X = pd.read_csv('raw_data/test_data_X.csv')
+# y_test = pd.read_csv('raw_data/test_data_y.csv')
+
+# X = X.drop(columns='Unnamed: 0')
+# X = np.array(X)
+# X = X.reshape(1413,24,5)
 
 
-print(new_model.predict(x_test[0]))
+
+def get_data():
+    X = pd.read_csv('raw_data/test_data_X.csv')
+    y_test = pd.read_csv('raw_data/test_data_y.csv')
+    X = X.drop(columns='Unnamed: 0')
+    X = np.array(X)
+    X = X.reshape(1413,24,5)
+    new_model  = load_model('gs://horseracingproject/base_model')
+    return X, y_test, new_model
