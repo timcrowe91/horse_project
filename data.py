293055a30d03c74b77ed99f599e45e2c4d028cad
Rex Elardo <rexelardo@gmail.com>
2021-04-08 17:24:47 -0400

    the program successfully loads the model from Google CLoud, cannot wait for an actual good model to come in and integrate soon

--- data.py
+++ data.py
@@ -1,12 +1,15 @@
 import pandas as pd
-
-x_test = pd.read_csv('raw_data/test_data_X.csv')
+import numpy as np
+X = pd.read_csv('raw_data/test_data_X.csv')
 y_test = pd.read_csv('raw_data/test_data_y.csv')
 
+X = X.drop(columns='Unnamed: 0')
 
+X = np.array(X)
+X = X.reshape(1413,24,5)
+X
 from tensorflow.keras.models import load_model
-
-new_model  = load_model('https://console.cloud.google.com/storage/browser/horseracingproject/base_model')
+new_model  = load_model('gs://horseracingproject/base_model')
 
 
 print(new_model.predict(x_test[0]))
