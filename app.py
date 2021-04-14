0bc323c47711f9bde35dff5b07eac82b4a000407
Rex Elardo <rexelardo@gmail.com>
2021-04-14 17:12:00 -0400

    the team voted on a fancier image

--- app.py
+++ app.py
@@ -8,15 +8,29 @@ import numpy as np
 # import requests as re
 # import json
 
-st.markdown(
-    """
+import base64
+
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
+    page_bg_img = '''
     <style>
-    .reportview-container {
-        background: url("https://media.istockphoto.com/photos/horse-racing-picture-id544653476?k=6&m=544653476&s=612x612&w=0&h=QOdC-WCOHGmIQi5lklzN6pEP63yEHoR0cBZXWIT3oZE=")
+    body {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
     }
     </style>
-    """,
-    unsafe_allow_html=True)
+    ''' % bin_str
+    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
+
+set_png_as_page_bg('horse-race.jpg')
 
 st.markdown("""# Horse Arbitrator
 ## 🐎🐎🐎 Calculates the future odds of each horse perfectly 🐴🐴🐴
